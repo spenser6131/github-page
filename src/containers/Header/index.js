1ed40bd808d059688da3d9f8fd9aa139ddb69373
spenser6131 <brinkman.spenser@gmail.com>
2021-04-14 11:09:12 -0700

    test

--- src/containers/Header/index.js
+++ src/containers/Header/index.js
@@ -50,6 +50,7 @@ class Header extends Component {
           <a href="https://www.codewars.com/users/spenser6131">
             <img src={codewarsIcon} alt="Codewars: spenser6131" width="25px" height="25px" />
           </a>
+          TEST
         </div>
 
       </div>
